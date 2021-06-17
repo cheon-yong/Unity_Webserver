af806f2e5778564675b5bce6a0b443caebb98696
yongjun <yes1312@naver.com>
2021-06-17 13:05:33 +0900

    Async/Await test

---
+++ Async Await/Async Await/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Async_Await
+{
+    // async/await
+    // 비동기 프로그래밍 문법
+    
+
+    class Program
+    {
+        static Task Test()
+        {
+            Console.WriteLine("Start Test");
+            Task t = Task.Delay(3000);
+            Console.WriteLine("End Test");
+            return t;
+        }
+
+        static async Task<int> TestAsync()
+        {
+            Console.WriteLine("Start TestAsync");
+            await Task.Delay(3000);
+            Console.WriteLine("End TestAsync");
+            return 1;
+        }
+
+        static async Task Main(string[] args)
+        {
+            int ret = await TestAsync();
+
+            Console.WriteLine("while start");
+            Console.WriteLine(ret);
+
+            while(true)
+            {
+
+            }
+        }
+    }
+}
