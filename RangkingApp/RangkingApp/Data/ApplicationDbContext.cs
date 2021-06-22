9d853d91ebe7cf7c16cf552063d2ee3fc69768e3
yongjun <yes1312@naver.com>
2021-06-22 16:24:32 +0900

    Ranking

--- RangkingApp/RangkingApp/Data/ApplicationDbContext.cs
+++ RangkingApp/RangkingApp/Data/ApplicationDbContext.cs
@@ -3,11 +3,13 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using RangkingApp.Data.Models;
 
 namespace RangkingApp.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<GameResult> GameResults { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
