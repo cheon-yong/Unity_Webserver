f899741132105b67545047eca69de3c03beb3c69
yongjun <yes1312@naver.com>
2021-07-14 23:33:03 +0900

    WebApi

--- RankingApp/WebApi/Controllers/RankingController.cs
+++ RankingApp/WebApi/Controllers/RankingController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SharedData.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApi.Data;
 
 namespace WebApi.Controllers
 {
@@ -10,5 +12,76 @@ namespace WebApi.Controllers
     [ApiController]
     public class RankingController : ControllerBase
     {
+        ApplicationDbContext _context;
+
+        public RankingController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // Create
+        [HttpPost]
+        public GameResult AddGameResult([FromBody]GameResult gameResult)
+        {
+            _context.GameResults.Add(gameResult);
+            _context.SaveChanges();
+
+            return gameResult;
+        }
+
+        // Read
+        [HttpGet]
+        public List<GameResult> GetGameResults()
+        {
+            List<GameResult> results = _context.GameResults
+                .OrderByDescending(item => item.Score)
+                .ToList();
+
+            return results;
+        }
+
+        [HttpGet("{id}")]
+        public GameResult GetGameResult(int id)
+        {
+            GameResult result = _context.GameResults
+                .Where(item => item.Id == id)
+                .FirstOrDefault();
+
+            return result;
+        }
+        // Update
+        [HttpPut]
+        public bool UpdateGameResult([FromBody] GameResult gameResult)
+        {
+            var findResult = _context.GameResults
+                .Where(x => x.Id == gameResult.Id)
+                .FirstOrDefault();
+
+            if (findResult == null)
+                return false;
+
+            findResult.UserName = gameResult.UserName;
+            findResult.Score = gameResult.Score;
+            _context.SaveChanges();
+
+            return true;
+        }
+
+        // Delete
+        [HttpDelete("{id}")]
+        public bool DeleteGameResult(int id)
+        {
+            var findResult = _context.GameResults
+                .Where(x => x.Id == id)
+                .FirstOrDefault();
+
+            if (findResult == null)
+                return false;
+
+            _context.GameResults.Remove(findResult);
+            _context.SaveChanges();
+
+            return true;
+        }
     }
 }
